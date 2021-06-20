ccf15c87098ddcad4d88404ce111dad07e639f44
Gabriel Evan <evan@archlinux.localdomain>
2021-06-20 12:11:44 +0700

    Added script

---
+++ Gnome/GNOME3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo systemctl enable gdm
+/bin/echo -e "REBOOTING IN 5.." && sleep 1 &&  echo "4.." && sleep 1 && echo "3.." && sleep 1 && echo "2.." && sleep 1 && echo "1.." && sleep 1
+sudo reboot
